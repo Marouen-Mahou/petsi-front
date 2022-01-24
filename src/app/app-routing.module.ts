76b578ce0a0f3f310da4d4d8dc302951ddc7ba8e
Marouen-Mahou <mahou.marouen@gmail.com>
2022-01-24 23:05:44 +0100

    navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppUserLayoutComponent } from './app-user-layout/app-user-layout.component';
 import { PetProfileComponent } from './pet-profile/pet-profile.component';
 
 const routes: Routes = [
-  { path: 'pet', component: PetProfileComponent}
+  { path: 'user', component: AppUserLayoutComponent, children: [
+    { path: 'pet', component: PetProfileComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon'
 import { MatTableModule } from '@angular/material/table'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 
 import { PetProfileVaccineComponent } from './pet-profile-vaccine/pet-profile-vaccine.component'
@@ -22,6 +26,8 @@ import { PetProfileFoodComponent } from './pet-profile-food/pet-profile-food.com
 import { PetProfileBathComponent } from './pet-profile-bath/pet-profile-bath.component';
 import { PetProfileAppointmentComponent } from './pet-profile-appointment/pet-profile-appointment.component';
 import { PetProfileWeightComponent } from './pet-profile-weight/pet-profile-weight.component';
+import { AppSideNavComponent } from './app-side-nav/app-side-nav.component';
+import { AppUserLayoutComponent } from './app-user-layout/app-user-layout.component';
 
 
 @NgModule({
@@ -33,6 +39,8 @@ import { PetProfileWeightComponent } from './pet-profile-weight/pet-profile-weig
     PetProfileBathComponent,
     PetProfileAppointmentComponent,
     PetProfileWeightComponent,
+    AppSideNavComponent,
+    AppUserLayoutComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,7 +50,11 @@ import { PetProfileWeightComponent } from './pet-profile-weight/pet-profile-weig
     MatListModule,
     MatExpansionModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
