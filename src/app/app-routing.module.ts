312fdc18ff299a3f10e047c6e2a7e24bf8d14db3
Bahri-Wala <bahriwala@gmail.com>
2022-02-14 16:43:26 +0100

    checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SuccessComponent } from './success/success.component';
 import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent},
   { path: 'success', component: SuccessComponent},
-  { path: '**', component: NotFoundComponent }
+  { path: '', component: LoginComponent, canActivate:[NotAuthGuard] },
+  { path: '**', component: NotFoundComponent },
 ]
 
 @NgModule({
